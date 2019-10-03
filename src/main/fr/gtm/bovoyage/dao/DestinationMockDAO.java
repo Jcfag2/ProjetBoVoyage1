34958f8c3b90f41b5bbd16e23aa68f0b80e637e0
jcfag2 <j.fagothey@esff.fr>
2019-10-03 11:49:30 +0200

    creation de la methode create().

--- src/main/fr/gtm/bovoyage/dao/DestinationMockDAO.java
+++ src/main/fr/gtm/bovoyage/dao/DestinationMockDAO.java
@@ -1,14 +1,18 @@
 package fr.gtm.bovoyage.dao;
 
+import java.util.ArrayList;
 import java.util.List;
-
 import fr.gtm.bovoyage.entities.DatesVoyage;
 import fr.gtm.bovoyage.entities.Destination;
 
 public class DestinationMockDAO implements DestinationDAO {
+	private List<Destination> destinations = new ArrayList<Destination>();
+	private static long idCpt = 0;
 
 	@Override
 	public void create(Destination d) {
+		d.setId(++idCpt);
+		destinations.add(d);
 	}
 
 	@Override
