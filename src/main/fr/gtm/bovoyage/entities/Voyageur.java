77f4476fa39663625bedde60d1bb286ce7790ea0
deniskucuk78cd <denis.kucuk.pro@gmail.com>
2019-10-03 10:15:55 +0200

    Création des classes - zainab

---
+++ src/main/fr/gtm/bovoyage/entities/Voyageur.java
@@ -0,0 +1,60 @@
+package fr.gtm.bovoyage.entities;
+
+public class Voyageur {
+	private String civilite;
+	private String nom;
+	private String prenom;
+	private Date dateNaissance;
+	private long id;
+	
+	public Voyageur(String civilite, String nom, String prenom, Date dateNaissance, long id) {
+		this.civilite = civilite;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateNaissance = dateNaissance;
+		this.id = id;
+	}
+	
+	
+	
+	public String getCivilite() {
+		return civilite;
+	}
+	
+	public void setCivilite(String civilite) {
+		this.civilite = civilite;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getPrenom() {
+		return prenom;
+	}
+	
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+}
