8abf28daa56e6edba7f43e64374215e20922054d
jcfag2 <j.fagothey@esff.fr>
2019-10-03 10:59:24 +0200

    créartion des interfaces DAO - jc

---
+++ src/main/fr/gtm/bovoyage/dao/VoyageDAO.java
@@ -0,0 +1,12 @@
+package fr.gtm.bovoyage.dao;
+
+import fr.gtm.bovoyage.entities.Voyage;
+
+public interface VoyageDAO {
+
+	public void create(Voyage v);
+	public void delete(Voyage v);
+	public void update(Voyage v);
+	public Voyage getVoyageById(long id);
+
+}
