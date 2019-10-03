bde26459757d9e6abb423920396744a62fcd9323
deniskucuk78cd <denis.kucuk.pro@gmail.com>
2019-10-03 13:51:21 +0200

    lol

--- src/main/fr/gtm/main/Main.java
+++ src/main/fr/gtm/main/Main.java
@@ -13,29 +13,29 @@ public class Main {
 		char reponse;
 		DestinationFacade facade = new DestinationFacade(new DestinationMockDAO());
 
-		System.out.println("Bienvenue sur notre Agence BoVoyage.\nBoVoyage est la premiËre agence par ligne de commande sur la console.");
+		System.out.println("Bienvenue sur notre Agence BoVoyage.\nBoVoyage est la premi√®re agence par ligne de commande sur la console.");
 		Scanner scanner = new Scanner(System.in);
-		do {
-		System.out.println("\n\nVeuillez entrer une destination ‡ ajouter dans le Mock : ");
+		do {	//Boucle principale
+		System.out.println("\n\nVeuillez entrer une destination √† ajouter dans le Mock : ");
 		
 		
 		String inputString = scanner.nextLine();
 		String dest = inputString;
-		System.out.println("Vous avez entrÈ : "+inputString+"\n\nEntrez une description pour "+inputString+":");
+		System.out.println("Vous avez entr√© : "+inputString+"\n\nEntrez une description pour "+inputString+":");
 		String inputString2 = scanner.nextLine();
 		String desc = inputString2;
-		System.out.println("Vous avez entrÈ : "+ inputString2); 
+		System.out.println("Vous avez entr√© : "+ inputString2); 
 		
 		Destination d = new Destination(dest, desc);
 		facade.create(d);
 		
-		do {
-			System.out.println("Voulez vous crÈer une autre destination ? (O/N)");
+		do {	//On r√©p√®te cette boucle tant que l'utilisateur n'a pas rentr√© une autre destination.
+			System.out.println("Voulez vous cr√©er une autre destination ? (O/N)");
 			reponse = scanner.nextLine().charAt(0);
 			//scanner.close();
 		}while(reponse != 'N' && reponse != 'O');
 		}while(reponse == 'O');
-		System.out.println("Merci d'avoir utilisÈ notre console de voyage.\nA bientÙt pour de nouvelles aventures formidables!!");
+		System.out.println("Merci d'avoir utilis√© notre console de voyage.\nA bient√¥t pour de nouvelles aventures formidables!!");
 		scanner.close();
 		
 		
