637850e55995ce5542d0be564e8baccc2452f187
deniskucuk78cd <denis.kucuk.pro@gmail.com>
2019-10-03 11:12:11 +0200

    Création Destination Facade

---
+++ src/main/fr/gtm/bovoyage/facade/DestinationFacade.java
@@ -0,0 +1,35 @@
+package fr.gtm.bovoyage.facade;
+
+import java.util.List;
+
+import fr.gtm.bovoyage.entities.DatesVoyage;
+import fr.gtm.bovoyage.entities.Destination;
+
+public class DestinationFacade {
+	
+	private DestinationDAO dao; //lien vers le dao.
+	
+	public DestinationFacade(DestinationDAO dao) {
+		this.dao = dao;
+	}
+	
+	public void create(Destination d) {
+		dao.create(d);
+	}
+	
+	public List <Destination> getAllDestinations() {
+		return dao.getAllDestinations();
+	}
+	
+	public List <DatesVoyage> getDatesVoyages(Destination d) {
+		return dao.getDatesVoyages(d);
+	}
+	
+	public List <Destination> getDestinationByRegion(String region) {
+		return dao.getDestinationByRegion(region);
+	}
+	
+	public List <DatesVoyage> getDatesVoyages(long idDestination) {
+		return dao.getDatesVoyages(idDestination);
+	}
+}
